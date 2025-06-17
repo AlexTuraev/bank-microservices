106df6b6962efe7d32ee0eefb6d2e32f1f3dd5e5
Alex Turaev <altex_76@mail.ru>
2025-06-17 15:42:30 +0300

    подготовка модуля accounts

---
+++ accounts-app/src/main/java/org/tasks/accountsapp/AaccountsAppApplication.java
@@ -0,0 +1,13 @@
+package org.tasks.accountsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaccountsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaccountsAppApplication.class, args);
+    }
+
+}
---
+++ accounts-app/src/test/java/org/tasks/accountsapp/AaccountsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tasks.accountsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaccountsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
