50f100a5cd32d19b107212c248497243539885f1
Alex Turaev <altex_76@mail.ru>
2025-06-18 01:48:04 +0300

    настройка в accounts liquibase

--- accounts-app/src/main/java/org/tasks/accountsapp/AccountsAppApplication.java
+++ accounts-app/src/main/java/org/tasks/accountsapp/AccountsAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AaccountsAppApplication {
+public class AccountsAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AaccountsAppApplication.class, args);
+        SpringApplication.run(AccountsAppApplication.class, args);
     }
 
 }
