62ea4641e2cb96428882cb4687f0fb091d6e22a7
Alex Turaev <altex_76@mail.ru>
2025-07-01 01:35:31 +0300

    подготовка в cash-app /user/{login}/сash(2)

---
+++ commonsapp/src/main/java/org/tasks/commonsapp/CommonsappApplication.java
@@ -0,0 +1,13 @@
+package org.tasks.commonsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonsappApplication.class, args);
+    }
+
+}
---
+++ commonsapp/src/test/java/org/tasks/commonsapp/CommonsappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tasks.commonsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
