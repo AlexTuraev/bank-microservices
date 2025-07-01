5f18adfd167f514d3cfee25ed18c01d0add65dd5
Alex Turaev <altex_76@mail.ru>
2025-07-01 14:18:11 +0300

    небольшой рефакторинг

--- accounts-app/src/main/java/org/tasks/accountsapp/controller/Oauth2Controller.java
+++ accounts-app/src/main/java/org/tasks/accountsapp/controller/Oauth2Controller.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.tasks.aaacommons.dto.CashDto;
 import org.tasks.accountsapp.dto.ChangePswDto;
 import org.tasks.accountsapp.dto.ExtUsersDto;
 import org.tasks.accountsapp.dto.UserDto;
@@ -52,7 +53,7 @@ public class Oauth2Controller {
 
     @PostMapping("/change-cash")
     public ResponseEntity<?> changeCash(
-//            @RequestBody CashDto cashDto
+            @RequestBody CashDto cashDto
     ) {
 //        userService.changeCash(cashDto);
         return ResponseEntity.ok().build();
