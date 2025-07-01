58b795f4f4a524dbc8494fb3242ed9e46c8afbcf
Alex Turaev <altex_76@mail.ru>
2025-07-01 14:15:12 +0300

    рефакторинг aaa-commons

---
+++ aaa-commons/src/test/java/org/tasks/aaacommons/AaaCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tasks.aaacommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaaCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
